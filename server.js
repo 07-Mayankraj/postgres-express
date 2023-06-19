5a42f59d17d2983225d8cff6c6e168dfcafafa8e
07-Mayankraj <2003.mayank.raj@gmail.com>
2023-06-19 17:51:40 +0530

    segreated routes

--- server.js
+++ server.js
@@ -1,80 +1,29 @@
-const fastify = require('fastify')({logger : true});
-const db = require('./config/db');
+const fastify = require('fastify')({ logger: true });
+const controller = require('./controllers/user.controller');
 
-fastify.post('/users', async (request, reply) => {
-    const { name, email, age } = request.body;
 
-    try {
-        const user = await db('users').insert({ name, email, age }).returning('*');
-        reply.send(user);
-    } catch (error) {
-        reply.status(500).send(error);
-    }
-});
 
-fastify.get('/users', async (request, reply) => {
-    try {
-        const users = await db('users').select('*');
-        reply.send(users);
-    } catch (error) {
-        reply.status(500).send(error);
-    }
-});
 
-fastify.get('/users/:id', async (request, reply) => {
-    const { id } = request.params;
+fastify.get('/users', controller.getAllUser);
+fastify.get('/users/:id', controller.getUser);
 
-    try {
-        const user = await db('users').select('*').where({ id }).first();
-        if (!user) {
-            reply.status(404).send('User not found');
-        } else {
-            reply.send(user);
-        }
-    } catch (error) {
-        reply.status(500).send(error);
-    }
-});
+fastify.post('/users', controller.registerUser);
 
-fastify.put('/users/:id', async (request, reply) => {
-    const { id } = request.params;
-    const { name, email, age } = request.body;
+fastify.put('/users/:id', controller.updateUser);
 
-    try {
-        const user = await db('users').where({ id }).update({ name, email, age }).returning('*');
-        if (!user) {
-            reply.status(404).send('User not found');
-        } else {
-            reply.send(user);
-        }
-    } catch (error) {
-        reply.status(500).send(error);
-    }
-});
-
-fastify.delete('/users/:id', async (request, reply) => {
-    const { id } = request.params;
-
-    try {
-        const user = await db('users').where({ id }).del();
-        if (!user) {
-            reply.status(404).send('User not found');
-        } else {
-            reply.send('User deleted successfully');
-        }
-    } catch (error) {
-        reply.status(500).send(error);
-    }
-});
+fastify.delete('/users/:id', controller.deleteUser);
 
 // Start the server
+
+
+
 const start = async () => {
     try {
-        await fastify.listen({port: 3000})
+        await fastify.listen({ port: 3000 })
     } catch (error) {
         fastify.log.error(error)
         process.exit(1);
     }
-} 
+}
 
 start();
